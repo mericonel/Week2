0fe9607088065b0c40eb946b3b1a2f1de8a8b2b5
mericonel <mericonel@hotmail.com>
2016-09-13 02:50:21 -0400

    changes were done apparently

---
+++ New Unity Project 1/Assets/moveWithForce.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveWithForce : MonoBehaviour {
+
+	float acceleration = 30;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody> ().AddForce (Vector3.left * Input.GetAxis ("Horizontal") * acceleration );
+		GetComponent<Rigidbody> ().AddForce (Vector3.up * Input.GetAxis ("Vertical") * acceleration );
+
+
+
+	}
+}
---
+++ New Unity Project/Assets/TrapDoorSwitch.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapDoorSwitch : MonoBehaviour {
+
+	float rotationSpeed = -2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.T)) {
+			
+			transform.Rotate (Vector3.forward * rotationSpeed);
+		}
+	}
+}
